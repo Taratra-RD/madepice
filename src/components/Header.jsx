9f49b02744f091ce1c579dc91b4427da9be430e4
Pana <panarandri@gmail.com>
2023-07-17 16:05:36 +0300

    header aa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,15 @@ import "./css/Header.css"
 import logo from "./a.png"
 export default function Header() {
     return (
-        <div className="header">
+        <div className="header container">
             <ul className="header--list">
                 <li className="header--list--item">Nous</li>
                 <li className="header--list--item">Nos Produits</li>
-                <li className="header--list--item logo"><img src={logo} alt="" /></li>
+                <li className="header--list--item logo"><img src={logo}/></li>
                 <li className="header--list--item">Boutique</li>
                 <li className="header--list--item">Contact</li>
             </ul>
+            <div style={{}}><i className="fa fa-solid fa-facebook icon-bars"></i>Bars</div>
         </div>
     )
 }
