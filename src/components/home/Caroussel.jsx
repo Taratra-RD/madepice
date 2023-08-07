8aa97e5af828d9f8a7c49a37d6e3c6d1e52076cf
Pana <panarandri@gmail.com>
2023-08-08 00:54:00 +0300

    aza

--- src/components/home/Caroussel.jsx
+++ src/components/home/Caroussel.jsx
@@ -9,17 +9,20 @@ const Caroussel = () => {
     <div className="carousel-container"> {/* Updated className */}
       <Carousel>
         <Carousel.Item interval={3000}>
-          <center>
-            <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-              <h3 style={{fontFamily:'Canela'}}>LA PASSION DES SAVEURS</h3>
-              <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium, ea dignissimos tempore amet nam ducimus asperiores sequi natus dolore optio. Voluptates, tempore vel quos accusamus eligendi quibusdam.</p>
-              <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
-                En savoir plus
-              </span>
-            </div>
-          </center>
+          <Carousel.Caption>
+            <center>
+              <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
+                <h3 style={{fontFamily:'Canela'}}>LA PASSION DES SAVEURS</h3>
+                <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium, ea dignissimos tempore amet nam ducimus asperiores sequi natus dolore optio. Voluptates, tempore vel quos accusamus eligendi quibusdam.</p>
+                <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
+                  En savoir plus
+                </span>
+              </div>
+            </center>
+          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={2000}>
+        <Carousel.Caption>
           <center>
             <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
               <h3 style={{fontFamily:'Canela'}}>EXPLOSION DE SAVEURS AVEC NOS EPICES UNIQUES</h3>
@@ -29,8 +32,10 @@ const Caroussel = () => {
               </span>
             </div>
           </center>
+        </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
+        <Carousel.Caption>
           <center>
             <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
               <h3 style={{fontFamily:'Canela'}}>DECOUVREZ L'ART DES EPICES</h3>
@@ -40,6 +45,7 @@ const Caroussel = () => {
               </span>
             </div>
           </center>
+          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
