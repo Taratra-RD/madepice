cf3f5be0d49552ca54ef88c9ede8c7dca7875c52
Pana <panarandri@gmail.com>
2023-08-07 16:10:19 +0300

    carousel a faire

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,16 @@ import "./css/Footer.css"
 export default function Footer() {
     return (
         <div className="footer container-fluid" style={{color:'white'}}>
-            <p><i className="fa fas fa-phone" style={{color:'white'}}></i> 0345891851</p>
-            <p>Email : madepices@gmail.com</p>
-            <p><i className="fa fas fa-map" style={{color:'white'}}></i>Lot III L Ankaditoho</p>
-            
-            <div className="" style={{right:0}}>
-                <i className="fa fab fa-instagram fa-xl" style={{color:'white',marginRight:'0.5em'}}></i>
-                <i className="fa fab fa-facebook fa-xl" style={{color:'white',marginRight:'0.5em'}}></i>
+            <div className="container">
+                <p><i className="fa fas fa-phone" style={{color:'white'}}></i>  +261 34 58 918 51</p>
+                <p><i className="fa fas fa-envelope" style={{color:'white'}}></i>  madepices@gmail.com</p>
+                <p><i className="fa fas fa-location-dot" style={{color:'white'}}></i>Lot III L Ankaditoho</p>
+                <p><i className="fa fas fa-map" style={{color:'white'}}></i>Antananarivo 101</p>
+                <p>Tous droits résérvés <i className="fa far fa-copyright" style={{color:'white'}}></i> Copyright 2023</p>
+            </div>
+            <div className="container" style={{right:0}}>
+                <i className="fa fab fa-instagram fa-2xl" style={{color:'white',marginRight:'0.5em'}}></i>
+                <i className="fa fab fa-facebook fa-2xl" style={{color:'white',marginRight:'0.5em'}}></i>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,5 @@
+
+import { useRef,useEffect } from "react";
 import "./css/Home.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -9,6 +11,31 @@ import Card from "../components/home/Card";
 import { Col, Container, Row } from "react-bootstrap";
 
 function Home() {
+  const slideElements = useRef([]);
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('slide-left');
+          observer.unobserve(entry.target); // Stop observing once animated
+        }
+      });
+    }, observerOptions);
+
+    slideElements.current.forEach(element => {
+      observer.observe(element);
+    });
+
+    return () => {
+      observer.disconnect(); // Cleanup on unmount
+    };
+  }, []);
   return (
     <>
       <div className="home">
@@ -38,9 +65,13 @@ function Home() {
         </div>
         <div className="container-fluid img2">
           <Container>
-            <h2 style={{marginBottom:'0.5em'}}>DES EPICES AUX GOUTS AUTHENTIQUES, RECOLTEES DIRECTEMENT CHEZ LES CULTIVATEURS</h2>
-            <p style={{}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum animi unde itaque aut accusamus, repellat quae, tempore neque amet quaerat. Ipsam quibusdam sapiente vitae!</p>
-            <button className="btn" style={{background:'#f2e5d1',marginTop:'1.5em',color:'black',height:'3.5em',borderRadius:'9px',paddingLeft:'30px',paddingRight:'30px'}}>
+            <h2 className="slide-left" style={{marginBottom:'0.5em',fontFamily:'Canela'}} ref={el => slideElements.current.push(el)}>
+              DES EPICES AUX GOUTS AUTHENTIQUES, RECOLTEES DIRECTEMENT CHEZ LES CULTIVATEURS
+            </h2>
+            <p className="slide-left" style={{fontFamily:'Raleway'}} ref={el => slideElements.current.push(el)}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi illum animi unde itaque aut accusamus, repellat quae, tempore neque amet quaerat. Ipsam quibusdam sapiente vitae!
+            </p>
+            <button className="btn slide-left" style={{background:'#f2e5d1',marginTop:'1.5em',color:'black',height:'3.5em',borderRadius:'9px',paddingLeft:'30px',paddingRight:'30px',fontFamily:'Raleway',fontWeight:'500'}} ref={el => slideElements.current.push(el)}> 
               Découvrez notre histoire
             </button>
           </Container>
@@ -49,15 +80,14 @@ function Home() {
         </div>
         <div className="container-fluid img3">
           <Container style={{alignContent:'center'}}>
-            <h3 style={{textAlign:'center',paddingTop:'2em',paddingBottom:'0.7em'}}>NOS ENGAGEMENTS</h3>
+            <h3 style={{textAlign:'center',paddingTop:'2em',paddingBottom:'0.7em',fontFamily:'Canela'}}>NOS ENGAGEMENTS</h3>
             <div className="block">
             </div>
-            <h3 style={{textAlign:'center',paddingTop:'2em',paddingBottom:'0.7em'}}>NOS VALEURS</h3>
+            <h3 style={{textAlign:'center',paddingTop:'2em',paddingBottom:'0.7em',fontFamily:'Canela'}}>NOS VALEURS</h3>
             <div className="block">
             </div>
           </Container>
         </div>
-
         <Footer />
       </div>
     </>
