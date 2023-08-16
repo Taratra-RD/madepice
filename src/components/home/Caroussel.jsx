5954e84473a31276381fd22d937452c837b46324
Pana <panarandri@gmail.com>
2023-08-16 10:15:01 +0300

    loc

--- src/components/home/Caroussel.jsx
+++ src/components/home/Caroussel.jsx
@@ -8,7 +8,7 @@ const Caroussel = () => {
         <Carousel.Item interval={3000}>     
               <center>
               <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-                <h3 style={{fontFamily:'Canela'}}>LA PASSION DES SAVEURS</h3>
+                <h3 style={{fontFamily:'Canela Deck Trial'}}>LA PASSION DES SAVEURS</h3>
                 <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium, ea dignissimos tempore amet nam ducimus asperiores sequi natus dolore optio. Voluptates, tempore vel quos accusamus eligendi quibusdam.</p>
                 <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
                   En savoir plus
@@ -19,7 +19,7 @@ const Caroussel = () => {
         <Carousel.Item interval={3000}>     
               <center>
               <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-                <h3 style={{fontFamily:'Canela'}}>EXPLOSION DE SAVEURS AVEC NOS EPICES UNIQUES</h3>
+                <h3 style={{fontFamily:'Canela Deck Trial'}}>EXPLOSION DE SAVEURS AVEC NOS EPICES UNIQUES</h3>
                 <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium. Ad fd excepturi tenetur quidem, facere quia sit pariatur laudantium.</p>
                 <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
                   En savoir plus
@@ -30,7 +30,7 @@ const Caroussel = () => {
         <Carousel.Item interval={3000}>     
               <center>
               <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-                <h3 style={{fontFamily:'Canela'}}>DECOUVREZ L'ART DES EPICES</h3>
+                <h3 style={{fontFamily:'Canela Deck Trial'}}>DECOUVREZ L'ART DES EPICES</h3>
                 <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium. Afd rohdes jackass guys hueaz.</p>
                 <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
                   En savoir plus
