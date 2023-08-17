ef016e7460b62f99c6a360bde94b570ecdf42d07
Tsiky <andrianirinasoat@gmail.com>
2023-08-17 13:34:44 +0000

    erease caroussel

--- src/components/home/Caroussel.jsx
+++ src/components/home/Caroussel.jsx
@@ -37,30 +37,7 @@ const Caroussel = () => {
                 </span>
               </div>
               </center> 
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>     
-              <center>
-              <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-                <h3 style={{fontFamily:'Canela'}}>LA PASSION DES SAVEURS</h3>
-                <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium, ea dignissimos tempore amet nam ducimus asperiores sequi natus dolore optio. Voluptates, tempore vel quos accusamus eligendi quibusdam.</p>
-                <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
-                  En savoir plus
-                </span>
-              </div>
-              </center> 
-        </Carousel.Item>
-        <Carousel.Item interval={3000}>     
-              <center>
-              <div className="contenu-carousel" style={{ maxWidth: '30em' }}>
-                <h3 style={{fontFamily:'Canela'}}>LA PASSION DES SAVEURS</h3>
-                <p style={{fontFamily:'Raleway'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit corrupti, consequuntur at reiciendis nihil excepturi tenetur quidem, facere quia sit pariatur laudantium, ea dignissimos tempore amet nam ducimus asperiores sequi natus dolore optio. Voluptates, tempore vel quos accusamus eligendi quibusdam.</p>
-                <span className="btn" style={{background:'#f2e5d1',marginTop:'1em',color:'black',height:'2.5em',width:'10em',borderRadius:'8px',fontFamily:'Raleway'}}>
-                  En savoir plus
-                </span>
-              </div>
-              </center> 
-        </Carousel.Item>
-        
+        </Carousel.Item>        
       </Carousel>
     </div>
   );
