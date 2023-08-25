293a2b7f03671359944bbfbc397c970094ae1ca7
Pana <panarandri@gmail.com>
2023-08-25 22:06:33 +0300

    tsy aiko

--- src/components/Cardprolittl.jsx
+++ src/components/Cardprolittl.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import './css/Cardprolittl.css'
+import './css/CardProLittl.css'
 export default function CardproLittl({ product }) {
     return (
         <div className="card--pro--l">
             <div className="card-title">
-                <h3 style={{ fontSize: '15px', textTransform: 'uppercase', display: 'flex', justifyContent: 'center' }}>{product.name}</h3>
+                <h3 style={{ color: 'white', fontFamily: 'Canela Deck Trial' }}>{product.name}</h3>
+            </div>
+            <div className="card-info">
+                <h3 style={{ color: 'white', fontFamily: 'Canela Deck Trial' }}>Card</h3>
+                <p style={{ color: 'white', paddingTop: '15px', paddingBottom: '15px' }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum exercitationem enim vero eos delectus inventore ratione ab numquam perferendis nam.
+                </p>
+                <button className="btn" style={{ background: '#f2e5d1', borderRadius: '8px' }}>
+                    En savoir plus
+                </button>
             </div>
         </div>
     )
