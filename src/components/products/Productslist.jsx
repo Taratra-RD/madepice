8e4d235c0f3ee43d2ded7fdc2781fe710ced2b6b
Taratra-RD <taratra.access4@gmail.com>
2023-08-29 14:24:20 +0300

    version-1 product aborted

--- src/components/products/Productslist.jsx
+++ src/components/products/Productslist.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import './css/Productslist.css'
-import Cardproduct from './Cardproduct'
 
-export default function Productslist({ selectedLetter, groupedProducts }) {
+export default function Productslist() {
     return (
-        <div className='--products--list--card'>
-            <h2 style={{ fontStyle: 'italic', textTransform: 'capitalize', margin: '1rem' }}>{selectedLetter}</h2>
-            <div className='products--list--card'>
-                {groupedProducts.map(product => (
-                    <div key={product.name} className='--card'>
-                        <Cardproduct product={product} />
-                    </div>
-                ))}
-            </div>
+        <div>
+
         </div>
     )
 }
