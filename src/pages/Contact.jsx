e2984751c00b6a10b77f6465bfbd825ce746f0ba
Tsiky <andrianirinasiat@gmail.com>
2023-09-14 16:30:13 +0300

    done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () =>{
                         <div className="contact-header">
                             <Header/>
                         </div>
-                        <div className="title-1">
+                        <div className="title-1-contact">
                             CONTACT & FAQ
                         </div>
                     </div>
